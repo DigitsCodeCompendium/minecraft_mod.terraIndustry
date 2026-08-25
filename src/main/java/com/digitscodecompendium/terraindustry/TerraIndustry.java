dac81013c260aba4301c40546dfb180a2bddcfdd
Daniel <binaryclock03@gmail.com>
2026-08-25 00:03:19 -0400

    setup first mod and got it building

---
+++ src/main/java/com/digitscodecompendium/terraindustry/TerraIndustry.java
@@ -0,0 +1,19 @@
+package com.digitscodecompendium.terraindustry;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(TerraIndustry.MODID)
+public class TerraIndustry {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "terraindustry";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public TerraIndustry() {
+    }
+}
